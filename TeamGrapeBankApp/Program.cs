02abbd036d1030274650b266f3324410a9b63d82
qvarnstr0m <mqvarnstrom80@gmail.com>
2022-11-21 14:57:00 +0100

    Add project files.

---
+++ TeamGrapeBankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamGrapeBankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
